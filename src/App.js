511a0abc33ebcc39cc66f32eff242589125a644c
shahalam sharif <shahalam.shareef@gmail.com>
2022-05-07 13:50:31 +0600

    client-side initialized and tested

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
